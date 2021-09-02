9c4e9f65a24f13216d9479f6d10a46dbf9e1ad7d
Tomas Gregoret <tomasfgregoret@gmail.com>
2021-09-02 20:57:33 -0300

    Install requirements

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# activate the environment
+# create and activate an environment called env
+virtualenv env
 source env/bin/activate
+
+# install requirements
+pip install -r requirements.txt
 
 # run local server
 python todoapp.py
