be2250d16cf2cfdc6e0f7e7ca091efecbd0e2134
Tomas Gregoret <tomasfgregoret@gmail.com>
2021-09-08 17:46:16 -0300

    Modified to access db through the DbObject

--- backend/todoapp/logic.py
+++ backend/todoapp/logic.py
@@ -1,78 +1,52 @@
-from flask import request, jsonify, make_response
+from flask import jsonify, make_response, request
 from todoapp import app
-from todoapp.database import Item, db
+from todoapp.database import DbObject as db
+
 
 @app.route('/get_items')
 def getItems():
-    items_list = Item.query.all()
-
-    items = []    
-
-    for item in items_list:
-        item_data = {}
-        item_data['id'] = item.id
-        item_data['content'] = item.content
-        item_data['completed'] = item.completed
-        items.append(item_data)
-    
-    response = jsonify(
-        {
-            "total": len(items_list),
-            "items": items
-        }
-    )
-
-    return make_response(response, 200) 
+    result = jsonify(db.query_items())
+    return make_response(result, 200) 
 
 
 @app.route('/add_item', methods=['POST'])
 def addItem():
     item_content = request.args.get('content')
-    new_item = Item(content=item_content)
+    success = db.add_item(item_content)
 
-    try:
-        db.session.add(new_item)
-        db.session.commit()
+    if success:
         return make_response(jsonify('New item created.'), 201)
-    except:
+    else:
         return make_response(jsonify('There was an issue creating the new item.'), 500)
 
 
 @app.route('/delete_item/<id>', methods=['DELETE'])
 def deleteItem(id):
-    item_to_delete = Item.query.get_or_404(id) 
+    success = db.delete_item(id)
 
-    try:
-        db.session.delete(item_to_delete)
-        db.session.commit()
+    if success:
         return make_response(jsonify('Item deleted.'), 200)
-    except:
+    else:
         return make_response(jsonify('There was an issue deleting the item.'), 500)
 
 
 @app.route('/update_item/<id>', methods=['PUT'])
 def updateItem(id):
-    item_to_update = Item.query.get_or_404(id)
     new_content = request.args.get('content')
-    
-    item_to_update.content = new_content 
+    success = db.update_item(id, new_content)
 
-    try:
-        db.session.commit()
+    if success:
         return make_response(jsonify('Item updated.'), 200)
-    except:
+    else:
         return make_response(jsonify('There was an issue updating the item.'), 500)
 
 
 @app.route('/change_state/<id>/<state>', methods=['PUT'])
 def changeState(id, state):
-    item_to_update = Item.query.get_or_404(id)
     new_state = int(state) 
+    success = db.change_state(id, new_state)
 
-    item_to_update.completed = new_state
-
-    try:
-        db.session.commit()
+    if success:
         return make_response(jsonify('Item status updated'), 200)
-    except:
+    else:
         return make_response(jsonify('There was an issue updating the item state'), 500)
